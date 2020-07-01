b9820b915047c5d1c97563ac33321b700c959b2e
Piotr Kristowski <cybszot@wp.pl>
2020-07-01 18:08:23 +0200

    Update index.js

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function Hero({ images, title, button, }) {
+export default function Hero({ images, title, subtitle, button }) {
     return (
-        <div>
-            Hero
+        <div className={s.root}>
+            <div className={s.bg} style={{ backgroundImage: `url(${images[0]})` }} />
+            <div className="d-flex align-items-center h-100">
+                <div className={s.text}>
+                    <h2>{title}</h2>
+                    <div className="mt-4 mb4">{subtitle}</div>
+                    <button className="btn btn-primary btn-lg">{button}</button>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Hero
